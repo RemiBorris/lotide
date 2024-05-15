7dffa23c89ad4ff6b13106ae3e10877159c2775d
remiborris <remiborris@gmail.com>
2024-05-15 19:41:47 -0300

    modified code to exclude return sequences which were not functional

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-    return;
   } else console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-  return;
 };
 
 // Test Code
