97dc4b1eae8aa054459bb579d999756f04738683
remiborris <remiborris@gmail.com>
2024-05-15 19:39:53 -0300

    Modified assertEqual.js concatenation to use template litterals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅  Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
     return;
-  } else console.log("🛑🛑🛑  Assertion Failed: " + actual + " !== " + expected);
+  } else console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
   return;
 };
 
