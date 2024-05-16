ea92024f8bf6a17c9dab2771352ce00f9b2b7789
remiborris <remiborris@gmail.com>
2024-05-16 18:00:22 -0300

    fix eslint issues in file

--- head.js
+++ head.js
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 // function to return the first item in an array
 const head = function(array) {
   return array[0];
-}
+};
 // Test Code
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
