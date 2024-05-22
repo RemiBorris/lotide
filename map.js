ee5702e781224b43dfabada1f12e1575a93f4b2f
remiborris <remiborris@gmail.com>
2024-05-22 13:01:32 -0300

    fix eslint issues

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function (array, callback) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
@@ -33,5 +33,5 @@ const assertArraysEqual = function(arr1, arr2) {
 //Test code
 
 assertArraysEqual(map(words, (word) => word.length),[6, 7, 2, 5, 3]);
-assertArraysEqual(map(words, (word) => word[word.length-1]),["d", "l", "o", "r", "m"]);
+assertArraysEqual(map(words, (word) => word[word.length - 1]),["d", "l", "o", "r", "m"]);
 assertArraysEqual(map(words, (word) => word.indexOf("o")),[2, 1, 1, 3, 1]);
