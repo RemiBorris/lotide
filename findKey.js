4069c026ec2df2b61a515ad817332fa45647644a
remiborris <remiborris@gmail.com>
2024-05-22 14:50:26 -0300

    create findKey.js function to use callback and return the key of the matching element it finds

---
+++ findKey.js
@@ -0,0 +1,35 @@
+// Test Function Code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Function Code
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item]))
+      return item;
+  }
+};
+
+// Test Code
+const data1 =   {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+
+const data2 = { color: "red", size: "medium" };
+const data3 = { a: 1, b: [2, 3, 4] };
+const data4 = { a: 1, b: 2, c: 3 };
+
+assertEqual(findKey(data1, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(data2, x => x === "red"), "color");
+assertEqual(findKey(data3, x => Array.isArray(x)), "b");
+assertEqual(findKey(data4, x => x === 3), "c");
