5b43c2704becbbe6632f1c059672183f996c783f
remiborris <remiborris@gmail.com>
2024-06-03 14:47:06 -0300

    refactor code to use mocha chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,16 @@
 const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 // Test Code
 
-// Test to ensure original array length is not modified
-const test1 = ["testing","this","out"];
-tail(test1);
-assertEqual(test1.length, 3);
-// Test that an empty array returns an empty array
-const test2 = [];
-assertEqual(tail(test2).length, 0);
-// Test 1 item array should return an empty array
-const test3 = ["testing"];
-assertEqual(tail(test3).length, 0);
-// Check that the first element of the returned array is the second element of the original array
-assertEqual(tail(test1)[0], "this");
+describe('#tail', () => {
+  it('return an empty array when receiving []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it('return an empty array when receiving 1 item', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it('check that the first element of the returned array is the second element of the origina array', () => {
+    assert.deepEqual(tail(["testing","this","out"])[0], "this");
+  });
+});
