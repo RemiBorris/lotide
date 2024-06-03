002b559c89215b0e33abddc64d3803741014d2fd
remiborris <remiborris@gmail.com>
2024-06-03 13:52:10 -0300

    remove test functions, export middle function to test file to test separately

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-// Function to compare 2 arrays
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-};
-
-// Function to compare 2 arrays and console log
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅  Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
 // Function to return middle of an array
 const middle = function(array) {
   const middleArray = [];
@@ -36,10 +14,4 @@ const middle = function(array) {
   return middleArray;
 };
 
-//Tests
-assertArraysEqual(middle([1,2,3,4]),([2,3]));
-assertArraysEqual(middle([1,2,3]),([2]));
-assertArraysEqual(middle([1]),([]));
-assertArraysEqual(middle([1,2]),([]));
-assertArraysEqual(middle([1,2,3,4,5]),([3]));
-assertArraysEqual(middle([1,2,3,4,5,6]),([3,4]));
+module.exports = middle;
