e92f18087730b6d491a5ea758575a4860ec6caa1
remiborris <remiborris@gmail.com>
2024-06-03 13:34:49 -0300

    add tailTest.js code with import functionality to not copy/paste code

---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+// Test Code
+
+// Test to ensure original array length is not modified
+const test1 = ["testing","this","out"];
+tail(test1);
+assertEqual(test1.length, 3);
+// Test that an empty array returns an empty array
+const test2 = [];
+assertEqual(tail(test2).length, 0);
+// Test 1 item array should return an empty array
+const test3 = ["testing"];
+assertEqual(tail(test3).length, 0);
