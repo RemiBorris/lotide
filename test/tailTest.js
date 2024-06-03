522e93689df75fd1a29ffb6060c4206090612649
remiborris <remiborris@gmail.com>
2024-06-03 13:37:21 -0300

    add testing recommendations

--- test/tailTest.js
+++ test/tailTest.js
@@ -12,3 +12,5 @@ assertEqual(tail(test2).length, 0);
 // Test 1 item array should return an empty array
 const test3 = ["testing"];
 assertEqual(tail(test3).length, 0);
+// Check that the first element of the returned array is the second element of the original array
+assertEqual(tail(test1)[0], "this");
