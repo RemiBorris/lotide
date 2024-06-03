372b810096876bfaf8930b85521dcd898f7fffc5
remiborris <remiborris@gmail.com>
2024-06-03 13:41:04 -0300

    remove copy/paste test code, export function to test folder to be tested

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-// Function Implementation to check if we receive the expected value
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-};
-
 // Function to compare 2 arrays
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -17,11 +10,4 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-// Test Case for different length string
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should FAIL
-// Test Case for matching arrays
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// Test Case for non matching arrays
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should FAIL
-//Test Case for number arrays vs string array
-assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false); // => should FAIL
+module.exports = eqArrays;
