240237b477ea54c637c509a6f435952e67dad449
remiborris <remiborris@gmail.com>
2024-06-03 13:22:39 -0300

    create test repository and files of head.js and assertEqual.js to import and use functions and not copy code between files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require("../assertEqual");
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, "2");
+assertEqual("same", "same");
+assertEqual("same", "different");
+assertEqual(42, 25);
